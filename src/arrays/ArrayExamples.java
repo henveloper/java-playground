1087c194eceab72139b601ca8803e88537c12d8a
_en <ben.pmt2d@gmail.com>
2022-05-30 12:35:25 +0800

    initial commit

---
+++ src/arrays/ArrayExamples.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class ArrayExamples {
+    public static void main(String[] args) {
+        int[] arr = new int[6];
+
+        System.out.println(arr);
+        for (int i = 0; i < 6; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
