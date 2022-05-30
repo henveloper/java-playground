07f0b7a84c295d8ac68165c8e11d788f2c86bc1e
_en <ben.pmt2d@gmail.com>
2022-05-30 16:03:49 +0800

    map

---
+++ src/MapExamples.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+public class MapExamples {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("a", 1);
+        map.put("b", 2);
+        map.put("c", 3);
+
+        for (Entry<String, Integer> entry : map.entrySet()) {
+            entry.getKey();
+            entry.getValue();
+        }
+    }
+}
